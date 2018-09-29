508b924dc5a225e93196380fbec53a380aa1c7bf
Luke Evans <luevans248@gmail.com>
2018-09-28 18:11:07 -0700

    added constructor, contains, and removeFront

--- ArrayHeadTailListInterface.java
+++ ArrayHeadTailListInterface.java
@@ -6,6 +6,44 @@ public class ArrayHeadTailListInterface<T> implements HeadTailListInterface<T> {
   private T[] listArray;
   private int numberOfElements;
 
+  // Luke Evans
+  public ArrayHeadTailListInterface(int initialCapacity) {
+    if(initialCapacity < 1) {
+      initialCapacity = 1;
+    }
+
+    @SuppressWarnings(“unchecked”)
+    listArray = (T) new Object[initialCapacity];
+    numberOfElements = 0;
+  }
+
+  public int contains(T anEntry) {
+    for(int i=0; i < numberOfElements; i++) {
+      if(listArray[i].equals(anEntry)) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  public T removeFront() {
+    if(numberOfElements > 0) {
+      T removedElement = listArray[0];
+      for(int i=0; i < numberOfElements; i++) {
+        if(i + 1 != numberOfElements) {
+          listArray[i] = listArray[i+1];
+        } else {
+          listArray[i] = null;
+        }
+      }
+      numberOfElements -= 1;
+      return removedElement;
+    } else {
+      return null;
+    }
+  }
+
   // Joshua Hansen
 
   public boolean isEmpty() {
